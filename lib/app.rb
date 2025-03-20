50dd9f16e73c3fc9d0f3edeccf4e807ccb00dd2c
Blake Astley <astley92@hotmail.com>
2025-03-20 17:26:21 +1100

    Add app class

---
+++ lib/app.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class App
+  def run
+    puts "Running"
+  end
+end
---
+++ main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative("lib/app")
+
+app = App.new
+app.run
